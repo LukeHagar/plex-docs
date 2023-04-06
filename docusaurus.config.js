45baeb0d085ca5a4fc125b0f92ced5c6f047307e
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2023-04-06 15:54:03 -0500

    corrected blog editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/LukeHagar/plex-docs/tree/main/blog",
+          editUrl: "https://github.com/LukeHagar/plex-docs/tree/main",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
